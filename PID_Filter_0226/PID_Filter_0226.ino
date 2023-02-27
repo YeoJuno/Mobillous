0b696f0870353e4afc8c2c59a49c07300b78481f
Hasem <legobox137@naver.com>
2023-02-27 16:20:08 +0900

    0227 tuned filter added

--- PID_Filter_0226/PID_Filter_0226.ino
+++ PID_Filter_0226/PID_Filter_0226.ino
@@ -1,5 +1,6 @@
 #include <mobis_io.h>
 #include <mobis_motor.h>
+#include <SoftwareSerial.h> 
 
 Motor motor_L(3), motor_R(4);
 
@@ -10,14 +11,14 @@ int sensor_index = 0; // Index for storing sensor data in array
 uint8_t max_speed = 250;   
 uint8_t min_speed = 160;   
 uint8_t speed_diff = 0;   
-int kp = 204;   
-int ki = 255;   
-int kd = 2000;   
+int kp = 20;   
+int ki = 0.8;   
+int kd = 2;   
 int last_error = 0;   
 int error = 0;   
 int total_error = 0;   
 float total = 0;
-float filter_val = 0.3;
+float filter_val = 0.2;
 
 void setup() {
     motor_L.setSpeed(max_speed);                
@@ -49,8 +50,17 @@ void loop() {
     int output = kp * filtered_error + ki * total_error + kd * (filtered_error - last_error);
 
 
-    int left_speed = max_speed - 3* output;
-    int right_speed = max_speed + 4* output;
+    // Serial Plotter for PPT screenshot
+
+    Serial.println(error1);
+    Serial.print(" ");
+    Serial.println(filtered_error);
+    Serial.print(" ");
+    Serial.println(output);
+
+
+    int left_speed = max_speed - 4 * output;
+    int right_speed = max_speed + 4 * output;
 
     if (left_speed > max_speed) left_speed = max_speed;
     if (right_speed > max_speed) right_speed = max_speed;
@@ -58,19 +68,19 @@ void loop() {
     if (right_speed < min_speed) right_speed = min_speed;
 
     if ((filtered_error <= filter_val && filtered_error >= (-1) * filter_val) && (!(val1 == 0 && val2 == 0))) {
-    // Both sensors on the line or filter average between 0.3 and -0.3 -> drive straight
+    // Both sensors on the line or filter average between 0.2 and -0.2 -> drive straight
         motor_L.setSpeed(max_speed);
         motor_R.setSpeed(max_speed);
         motor_L.run(FORWARD); 
         motor_R.run(FORWARD);
-  }
+    }
     else if (val1 == 1 && val2 == 0) {
     // Left sensor on the line, turn right
         motor_L.setSpeed(left_speed - speed_diff);
         motor_R.setSpeed(min_speed);
         motor_L.run(FORWARD); 
         motor_R.run(BACKWARD);
-  }
+    }
      else if (val1 == 0 && val2 == 1){
     // Right sensor on the line, turn left
         motor_L.setSpeed(min_speed);
@@ -78,5 +88,10 @@ void loop() {
         motor_L.run(BACKWARD); 
         motor_R.run(FORWARD);
  
-  }
+    }
+    
+    
+
+
+
 }
